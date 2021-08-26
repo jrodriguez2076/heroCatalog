44175ed3a0bb43f7b16ee9bedda3fa8f9630f9d9
aniitasmith <66226368+aniitasmith@users.noreply.github.com>
2021-08-26 16:26:52 -0300

    creada funcion buscadora de heroes (#10)

--- heroes.js
+++ heroes.js
@@ -1,9 +1,9 @@
 heroes = [{
-        id: '01',
+        id: 01,
         name: 'SuperMan',
         alterEgo: 'Clark Kent',
         biography: 'Escondido bajo una vida comun de periodista en Metropi=olis, se encarga de mantener a salvo la ciudad de todo lo que ocurre ',
-        age: '80',
+        age: 80,
         placeOfBirth: 'Krypton',
         race: 'Kryptoniano',
         base: 'Metropolis',
@@ -11,11 +11,11 @@ heroes = [{
         publisher: 'DC Comics',
     },
     {
-        id: '02',
+        id: 02,
         name: 'Supertipo',
         alterEgo: 'Sr tipo',
         biography: 'Era una tipo normal que fue golpeado por muchas veces por una pandilla de tipos, absorbiendo todas sus habilidades conviertiendola en poderes',
-        age: '682',
+        age: 682,
         placeOfBirth: 'pluton',
         race: 'plutoniano',
         base: 'Personalandia',
@@ -23,11 +23,11 @@ heroes = [{
         publisher: 'SangelComic',
     },
     {
-        id: '03',
+        id: 03,
         name: 'Spiderman',
         alterEgo: 'Peter Benjamin Parker',
         biography: 'Estudiante comun de la universidad que emplea sus habilidades sobrehumanas, reminiscentes de una araña, para combatir a otros supervillanos que persiguen fines siniestros.',
-        age: '17',
+        age: 17,
         placeOfBirth: 'EEUU',
         race: 'Humano',
         base: 'New York',
@@ -35,11 +35,11 @@ heroes = [{
         publisher: 'Marvel',
     },
     {
-        id: '04',
+        id: 04,
         name: 'Wonder Woman',
         alterEgo: 'Diana Prince',
         biography: 'Es una princesa guerrera de las Amazonas. Está dotada de una amplia gama de poderes superhumanos y habilidades de combate de batalla superiores, gracias a sus dones obtenidos de los dioses y su amplio entrenamiento.',
-        age: '800',
+        age: 800,
         placeOfBirth: 'Temiscira',
         race: 'Amazona',
         base: 'EEUU',
@@ -47,11 +47,11 @@ heroes = [{
         publisher: 'DC Comics',
     },
     {
-        id: '05',
+        id: 05,
         name: 'Ironman',
         alterEgo: 'Anthony Edward Stark ',
         biography: 'Multimillonario magnate empresarial e ingenioso científico, quien sufrió una grave lesión en el pecho durante un secuestro y crea una armadura para salvar su vida y escapar del cautiverio. Más tarde, Stark desarrolla su traje a traves de su compañía, Industrias Stark, para proteger al mundo',
-        age: '53',
+        age: 53,
         placeOfBirth: 'EEUU',
         race: 'Humano',
         base: 'EEUU',
@@ -67,4 +67,15 @@ function agregarNuevoHeroe(heroeNuevo) {
     } else {
         return 'Error. El heroe a agregar debe ser de tipo objeto';
     }
+}
+
+function encontrarHeroe(id) {
+    let resultado = null;
+    for (heroe of heroes) {
+        if (heroe.id === id) {
+            resultado = heroe;
+            break;
+        }
+    }
+    return resultado
 }
