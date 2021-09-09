3af8b04bbb6374cfbdd31efb44d97f8362d44253
aniitasmith <aniitasmith1992@gmail.com>
2021-09-09 19:52:45 -0300

    comparar editorial

--- heroes.js
+++ heroes.js
@@ -109,3 +109,27 @@ function eliminarHeroe(id) {
         return 'Tienes ' + heroes.length + ' heroes en la lista global';
     }
 }
+
+function compararEditoral(id1, id2) {
+    if (id1 === id2) {
+        return 'No se puede comparar el mismo heroe.'
+    }
+    let heroe1 = encontrarHeroe(id1);
+    let heroe2 = encontrarHeroe(id2);
+    if (heroe1 === null) {
+        return `Error. Heroe ${id1} no encontrado.`
+    }
+    if (heroe2 === null) {
+        return `Error. Heroe ${id2} no encontrado.`
+    }
+    if (!!heroe1.publisher || !!heroe2.publisher) {
+        if (heroe1['publisher'].toLowerCase() === heroe2['publisher'].toLowerCase()) {
+            return `${heroe1.name} y ${heroe2.name} estan en la misma casa Editoria.`
+        }
+        if ((heroe1['publisher'].toLowerCase() != heroe2['publisher'].toLowerCase())) {
+            return `${heroe1.name} y ${heroe2.name} estan en diferentes casas Editoriales.`;
+        }
+    } else {
+        return 'Error. Editoral de uno de los Heroes no encontrada.'
+    }
+}
