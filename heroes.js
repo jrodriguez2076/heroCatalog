cfa2194a0a3a27062620ce93687e60cda6a60cf3
aniitasmith <66226368+aniitasmith@users.noreply.github.com>
2021-09-09 20:29:30 -0300

    Hero--16//-comparar editorial (#20)
    * comparar editorial
    
    * arreglo de errores2

--- heroes.js
+++ heroes.js
@@ -133,3 +133,26 @@ function compararEdad(id1, id2) {
         return 'Error. Edad de uno de los Heroes no encontrada.'
     }
 }
+
+function compararEditoral(id1, id2) {
+    if (id1 === id2) {
+        return 'No se puede comparar el mismo heroe.'
+    }
+    let heroe1 = encontrarHeroe(id1);
+    let heroe2 = encontrarHeroe(id2);
+    if (heroe1 === null) {
+        return `Error. Heroe ${id1} no encontrado.`
+    }
+    if (heroe2 === null) {
+        return `Error. Heroe ${id2} no encontrado.`
+    }
+    if (!!heroe1.publisher || !!heroe2.publisher) {
+        if (heroe1['publisher'].toLowerCase() === heroe2['publisher'].toLowerCase()) {
+            return `${heroe1.name} y ${heroe2.name} estan en la misma casa Editorial.`
+        } else {
+            return `${heroe1.name} y ${heroe2.name} estan en diferentes casas Editoriales.`;
+        }
+    } else {
+        return 'Error. Editoral de uno de los Heroes no encontrada.'
+    }
+}
