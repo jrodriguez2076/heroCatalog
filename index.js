a5d3ecdda87cbca4325682b0bb879ea12e1be21e
aniitasmith <aniitasmith1992@gmail.com>
2021-11-05 22:11:47 -0300

    crrar interaccion

--- index.js
+++ index.js
@@ -12,3 +12,44 @@ const encontrarHeroe = require('./encontrarHeroe.js');
 const guardarLista = require('./guardarLista');
 const presentarHeroe = require('./presentarHeroe');
 const calcularEdadNativa = require('./calcularEdadNativa');
+
+// PROCESS.ARGV DIVIDIR EL COMANDO CONVERTIDO EN ARREGLO PARA PODER ACCEDER A lAS FUNCIONES DESDE LA CONSOLA 
+const procesoHeroe = process.argv.slice(2);
+
+switch (procesoHeroe[0]) {
+    case "calcularTiempoDeDescanso":
+        console.log(calcularTiempoDeDescanso(parseInt(procesoHeroe[1])));
+
+    case "compararEdad":
+        console.log(compararEdad(parseInt(procesoHeroe[1]), parseInt(procesoHeroe[2])));
+        break;
+
+    case "compararEditoral":
+        console.log(compararEditorial(parseInt(procesoHeroe[1]), parseInt(procesoHeroe[2])));
+        break;
+
+    case "compararRaza":
+        console.log(compararRaza(parseInt(procesoHeroe[1]), parseInt(procesoHeroe[2])));
+        break;
+
+    case "eliminarHeroe":
+        console.log(eliminarHeroe(parseInt(procesoHeroe[1])));
+        break;
+
+    case "encontrarHeroe":
+        console.log(encontrarHeroe(parseInt(procesoHeroe[1])));
+        break;
+
+    case "presentarHeroe":
+        console.log(presentarHeroe(parseInt(procesoHeroe[1])));
+        break;
+
+    case "calcularEdadNativa":
+        console.log(calcularEdadNativa(parseInt(procesoHeroe[1])));
+        break;
+
+    default:
+        console.log("Funcion no encontrada");
+        break;
+
+}
