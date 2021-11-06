d3bfffc0d5b9e39b6a2e2fa24a36b15d827ca845
aniitasmith <66226368+aniitasmith@users.noreply.github.com>
2021-11-05 23:01:00 -0300

    Hero--28// nuevos atributos de los heroes (#35)
    * crear interaccion
    
    * cambio
    
    * nuevos atributos en heroes
    
    * cambios
    
    * cambios 2
    
    * cambios 3

--- presentarHeroe.js
+++ presentarHeroe.js
@@ -7,7 +7,7 @@ function presentarHeroe(id) {
     if (heroe === null) {
         return `Error. Heroe ${id} no encontrado.`;
     } else {
-        return `${heroe.name} pertenece a la raza ${heroe.race}, tiene una edad de ${heroe.age} años, nació en ${heroe.placeOfBirth}, actualmente vive en ${heroe.base}, bajo el nombre de ${heroe.alterEgo}.`
+        return `${heroe.name} pertenece a la raza ${heroe.race}, tiene una edad de ${heroe.age} años, nació en ${heroe.placeOfBirth}, actualmente vive en ${heroe.base}, bajo el nombre de ${heroe.alterEgo}. Dentro de sus atributos estan: Fuerza: ${heroe.fuerza}/100, Inteligencia: ${heroe.inteligencia}/100, Agilidad: ${heroe.agilidad}/100, Habilidades: ${heroe.habilidades} y su debilidad: ${heroe.debilidad} `
     }
 }
 
